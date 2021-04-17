1b67348e9005a1fe63a3534e02b6f89f4162ff63
Artit 'Art' Wangperawong <artitw@gmail.com>
2021-04-17 15:23:52 -0400

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="text2text",
-    version="0.0.9",
+    version="0.1.0",
     author="Artit Wangperawong",
     author_email="artitw@gmail.com",
     description="Text2Text: generate questions and summaries for your texts",
