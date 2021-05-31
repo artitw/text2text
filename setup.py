e7df5896271fe048109007872c909ee9637ea4f6
artitw <artitw@gmail.com>
2021-05-31 15:56:13 +0000

    Exclude padding states from embedding calculation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="text2text",
-    version="0.2.0",
+    version="0.2.1",
     author="Artit Wangperawong",
     author_email="artitw@gmail.com",
     description="Text2Text: Multilingual tokenization, translation, summarization, question generation, question answering, data augmentation, embedding, distance measurement",
--- text2text/embedder.py
+++ text2text/embedder.py
@@ -10,4 +10,11 @@ class Embedder(Translator):
     tokenizer.src_lang = src_lang
     encoded_inputs = tokenizer(input_lines, padding=True, return_tensors="pt")
     outputs = model.forward(**encoded_inputs, decoder_input_ids=encoded_inputs["input_ids"])
-    return np.mean(outputs.encoder_last_hidden_state.detach().numpy(), 1)
+    last_layer_states = outputs.encoder_last_hidden_state.detach().numpy()
+    input_ids = encoded_inputs["input_ids"].detach().numpy()
+    non_paddings = input_ids!=1
+    non_paddings = non_paddings.astype(int)
+    non_paddings = np.repeat(non_paddings, last_layer_states.shape[-1], axis=1)
+    non_paddings = non_paddings.reshape(last_layer_states.shape)
+    embeddings = np.average(last_layer_states, axis=1, weights=non_paddings)
+    return embeddings
