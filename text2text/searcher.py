65f815764cec588f8d50d4abf6ca13450498d59d
artitw <artitw@gmail.com>
2021-06-26 18:49:20 +0000

    Clean up Searcher

--- text2text/searcher.py
+++ text2text/searcher.py
@@ -14,13 +14,7 @@ class Searcher(t2t.Transformer):
       queries = [queries]
     queries = search_object.transform(queries, src_lang='en', output="matrix", use_idf=False)
     
-    if type(queries[0]) is dict and type(index[0]) is dict:
-      scores = np.zeros((len(queries), len(index)))
-      for i, q in enumerate(queries):
-        for j, d in enumerate(index):
-            for tk in q:
-              scores[i,j] += q.get(tk,0)*d.get(tk,0)
-    elif type(queries) is np.ndarray and type(index) is np.ndarray:
+    if type(queries) is np.ndarray and type(index) is np.ndarray:
       scores = np.matmul(queries,np.transpose(index))
     elif type(queries) is sp.csr_matrix and type(index) is sp.csr_matrix:
       col_diff = queries.shape[1]-index.shape[1]
