b9f7a94ebb387712d3dcb9e202409db33d60bd6e
artitw <artitw@gmail.com>
2021-06-30 03:20:17 +0000

    Remove redundant code

--- text2text/vectorizer.py
+++ text2text/vectorizer.py
@@ -14,7 +14,6 @@ class Vectorizer(t2t.Translator):
     last_layer_states = outputs.encoder_last_hidden_state.detach().numpy()
     input_ids = encoder_inputs["input_ids"].detach().numpy()
     non_paddings = input_ids!=1
-    non_paddings = non_paddings.astype(int)
     last_layer_states *= non_paddings.astype(int).reshape(*non_paddings.shape, -1)
     x = np.mean(last_layer_states, axis=1)
     x /= np.linalg.norm(x, axis=1).reshape(x.shape[0],-1)
