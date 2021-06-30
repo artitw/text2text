2029a55271bc03aa88fe9ca8b50eb2bd4678a9c8
artitw <artitw@gmail.com>
2021-06-30 03:30:12 +0000

    Simplify decoder_inputs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="text2text",
-    version="0.4.9",
+    version="0.5.0",
     author="Artit Wangperawong",
     author_email="artitw@gmail.com",
     description="Text2Text: Multilingual tokenization, embedding, search, translation, summarization, question generation, question answering, data augmentation, distance measurement",
--- text2text/vectorizer.py
+++ text2text/vectorizer.py
@@ -1,3 +1,4 @@
+import torch
 import numpy as np
 import text2text as t2t
 
@@ -9,12 +10,11 @@ class Vectorizer(t2t.Translator):
     model = self.__class__.model
     tokenizer.src_lang = src_lang
     encoder_inputs = tokenizer(input_lines, padding=True, return_tensors="pt")
-    decoder_inputs = tokenizer(['']*len(input_lines), return_tensors="pt")
-    outputs = model.forward(**encoder_inputs, decoder_input_ids=decoder_inputs["input_ids"])
+    outputs = model.forward(**encoder_inputs, decoder_input_ids=torch.zeros(len(input_lines),1,dtype=int))
     last_layer_states = outputs.encoder_last_hidden_state.detach().numpy()
     input_ids = encoder_inputs["input_ids"].detach().numpy()
     non_paddings = input_ids!=1
-    last_layer_states *= non_paddings.astype(int).reshape(*non_paddings.shape, -1)
+    last_layer_states *= non_paddings.astype(int).reshape(*non_paddings.shape,-1)
     x = np.mean(last_layer_states, axis=1)
     x /= np.linalg.norm(x, axis=1).reshape(x.shape[0],-1)
     return x
