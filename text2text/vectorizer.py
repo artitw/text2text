56e1b35148328a54b0a0c7346e7af8b1f59cebbd
artitw <artitw@gmail.com>
2023-07-06 04:23:31 +0000

    Vectorizer update

--- text2text/vectorizer.py
+++ text2text/vectorizer.py
@@ -1,32 +1,33 @@
 import torch
 import numpy as np
 import text2text as t2t
+from torch import Tensor
 from transformers import AutoTokenizer, AutoModel
 import sklearn.preprocessing as preprocessing
 
-def mean_pooling(model_output, attention_mask):
-  token_embeddings = model_output[0]
-  input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
-  return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+def average_pool(last_hidden_states: Tensor, attention_mask: Tensor) -> Tensor:
+  last_hidden = last_hidden_states.masked_fill(~attention_mask[..., None].bool(), 0.0)
+  return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
+
 
 class Vectorizer(t2t.Transformer):
 
-  def __init__(self, pretrained_model='sentence-transformers/all-mpnet-base-v2', batch_size=0):
+  def __init__(self, pretrained_model='intfloat/multilingual-e5-large', batch_size=0):
     self.__class__.tokenizer = AutoTokenizer.from_pretrained(pretrained_model)
     self.__class__.model = AutoModel.from_pretrained(pretrained_model)
     self.__class__.batch_size = batch_size
 
   def batch_embed(self, input_lines):
-    encoder_inputs = self.__class__.tokenizer(input_lines, padding=True, truncation=True, return_tensors='pt')
+    tokenizer = self.__class__.tokenizer
+    model = self.__class__.model
+    encoder_inputs = tokenizer(input_lines, max_length=512, padding=True, truncation=True, return_tensors='pt')
     with torch.no_grad():
-      model_output = self.__class__.model(**encoder_inputs)
-    X = mean_pooling(model_output, encoder_inputs['attention_mask'])
-    return preprocessing.normalize(X, norm='l2')
+      outputs = model(**encoder_inputs)
+    embeddings = average_pool(outputs.last_hidden_state, encoder_inputs['attention_mask'])
+    return preprocessing.normalize(embeddings, norm='l2')
 
   def transform(self, input_lines, src_lang='en', batch_process=False, **kwargs):
     input_lines = t2t.Transformer.transform(self, input_lines, src_lang=src_lang, **kwargs)
-    tokenizer = self.__class__.tokenizer
-    model = self.__class__.model
     batch_size = self.__class__.batch_size
     if batch_size > 0:
       embeddings = None
