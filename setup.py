8e40a2617ae45665517251d75e58b7111f2eeaa0
artitw <artitw@gmail.com>
2024-10-29 00:27:32 +0000

    assistant updates

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
   version="1.8.2",
   author="artitw",
   author_email="artitw@gmail.com",
-  description="Text2Text: Crosslingual NLP/G toolkit",
+  description="Text2Text Language Modeling Toolkit",
   long_description=long_description,
   long_description_content_type="text/markdown",
   url="https://github.com/artitw/text2text",
@@ -18,7 +18,7 @@ setuptools.setup(
     "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
   ],
-  keywords='multilingual crosslingual gpt chatgpt bert natural language processing nlp nlg text generation gpt question answer answering information retrieval tfidf tf-idf bm25 search index summary summarizer summarization tokenizer tokenization translation backtranslation data augmentation science machine learning colab embedding levenshtein sub-word edit distance conversational dialog chatbot llama rag',
+  keywords='multilingual gpt chatgpt bert natural language processing nlp nlg text generation gpt question answer answering information retrieval tfidf tf-idf bm25 search index summary summarizer summarization tokenizer tokenization translation backtranslation data augmentation science machine learning colab embedding levenshtein sub-word edit distance conversational dialog chatbot llama rag',
   install_requires=[
     'faiss-cpu',
     'flask',
